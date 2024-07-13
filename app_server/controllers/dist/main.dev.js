563f89308d20c77e8a5a4c04bb2f29711b72e997
Madhavir123 <22eg106b35@anurag.edu.in>
2024-07-13 11:06:37 -0700

    first commit

---
+++ app_server/controllers/dist/main.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var index = function index(req, res) {
+  res.render('index', {
+    title: 'Online Auction System'
+  });
+};
+
+module.exports = {
+  index: index
+};
+//# sourceMappingURL=main.dev.js.map
